ecf1ff95e255e1e3cfc689599d4ce6119f7065d6
LizaBurenok <51739285+LizaBurenok@users.noreply.github.com>
2020-03-24 13:51:01 +0300

    added file with jquery

---
+++ mainjQuery.js
@@ -0,0 +1,17 @@
+let mouseColorTracking = document.querySelector('.line')
+mouseColorTracking.onclick = function(){
+$(document).mousemove(function (e) {
+
+  let $width = ($(document).width()) / 255;
+  let $height = ($(document).height()) / 255;
+  var $pageX = parseInt(e.pageX / $width, 10);
+  var $pageY = parseInt(e.pageY / $height, 10);
+  let $a = $pageX,
+      $b = ($pageY - 255) * (-1),
+      $c = 0;
+  if ($pageX <= $pageY) {
+      $c = $pageY - $pageX;
+  }
+  $('.book').css('background-color', 'rgb('+$a+','+$b+','+$c+')');
+});
+}
